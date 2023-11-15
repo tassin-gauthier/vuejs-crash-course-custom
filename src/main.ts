c5e49dd9fc8682b6729dcc1088917d96173a4b85
Gauthier Tassin <gauthier.tassin.partner@decathlon.com>
2023-11-15 10:28:58 +0100

    feat(init): add other configs

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import app from "./modules/common-module/app.vue";
+import "./global.scss";
 
 createApp(app).use(createPinia()).mount("#app");
